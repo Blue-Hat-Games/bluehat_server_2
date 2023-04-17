9b35f507f6f234b10e658b5d596b4040199f78d9
Jung Seonghun <80201773+seonghun-dev@users.noreply.github.com>
2023-04-17 17:51:28 +0900

    Init Project

---
+++ src/main/kotlin/com/example/bluehat_server2/BluehatServer2Application.kt
@@ -0,0 +1,11 @@
+package com.example.bluehat_server2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BluehatServer2Application
+
+fun main(args: Array<String>) {
+    runApplication<BluehatServer2Application>(*args)
+}
---
+++ src/test/kotlin/com/example/bluehat_server2/BluehatServer2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bluehat_server2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BluehatServer2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
